d3c09dec4aa49420f592e651dad21d0a29e48893
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2021-10-23 16:05:14 +0200

    Update sortorder

--- src/Vendr.Contrib.PaymentProviders.Nets/Easy/NetsEasyOneTimeSettings.cs
+++ src/Vendr.Contrib.PaymentProviders.Nets/Easy/NetsEasyOneTimeSettings.cs
@@ -6,7 +6,7 @@ namespace Vendr.Contrib.PaymentProviders
     {
         [PaymentProviderSetting(Name = "Auto Capture",
             Description = "Flag indicating whether to immediately capture the payment, or whether to just authorize the payment for later (manual) capture.",
-            SortOrder = 2000)]
+            SortOrder = 5000)]
         public bool AutoCapture { get; set; }
     }
 }
